429a0e630d52cfc25f16ababde0ccd95694d0f3e
chyroc <chyroc@qq.com>
2021-09-24 00:15:08 +0800

    support link: https://news.ycombinator.com/newest

---
+++ internal/routers/ycombinator/newest/feed.go
@@ -0,0 +1,50 @@
+package ycombinator_newest
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chyroc/grss/internal/fetch"
+)
+
+func New(map[string]string) (*fetch.Source, error) {
+	return &fetch.Source{
+		Title:  "Hacker News - New Links",
+		Link:   "https://news.ycombinator.com/newest",
+		Method: http.MethodGet,
+		URL:    "https://news.ycombinator.com/newest",
+		MapReduce: func(obj interface{}) ([]*fetch.Item, error) {
+			doc, err := goquery.NewDocumentFromReader(strings.NewReader(obj.(string)))
+			if err != nil {
+				return nil, err
+			}
+
+			items := []*fetch.Item{}
+			doc.Find(".athing").Each(func(i int, selection *goquery.Selection) {
+				id := selection.AttrOr("id", "")
+				link := selection.Find("a.storylink").AttrOr("href", "")
+				title := strings.TrimSpace(selection.Find("a.storylink").Text())
+				next := selection.Next()
+				age := strings.TrimSpace(next.Find(".age").AttrOr("title", ""))
+				point := strings.TrimSpace(next.Find(".score").Text())
+				pubTime, _ := time.Parse("2006-01-02T15:04:05", age)
+				desc := fmt.Sprintf(`Article URL: %s
+
+Comments URL: https://news.ycombinator.com/item?id=%s
+
+Points: %s
+`, link, id, point)
+				items = append(items, &fetch.Item{
+					Title:       title,
+					Link:        link,
+					Description: desc,
+					PubDate:     pubTime,
+				})
+			})
+			return items, nil
+		},
+	}, nil
+}
--- internal/routers/ycombinator/newest/feed_test.go
+++ internal/routers/ycombinator/newest/feed_test.go
@@ -1,10 +1,10 @@
-package ycombinator_best_test
+package ycombinator_newest_test
 
 import (
 	"testing"
 
 	"github.com/chyroc/grss/internal/fetch"
-	"github.com/chyroc/grss/internal/routers/ycombinator/best"
+	"github.com/chyroc/grss/internal/routers/ycombinator/newest"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,11 +12,11 @@ import (
 func Test_Feed(t *testing.T) {
 	as := assert.New(t)
 
-	feed, err := fetch.Fetch(ycombinator_best.New, nil)
+	feed, err := fetch.Fetch(ycombinator_newest.New, nil)
 	as.Nil(err)
 
-	as.Equal("Hacker News - Top Links", feed.Title)
-	as.Equal("https://news.ycombinator.com/best", feed.Link)
+	as.Equal("Hacker News - New Links", feed.Title)
+	as.Equal("https://news.ycombinator.com/newest", feed.Link)
 	spew.Dump(feed)
 	as.True(len(feed.Items) > 0)
 	as.NotEqual(feed.Items[0].Link, feed.Items[1].Link)
