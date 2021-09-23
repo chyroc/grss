774349ff7614b4906755caac38f089e1ed6086d7
chyroc <chyroc@qq.com>
2021-09-23 11:43:00 +0800

    remove rss prefix

--- interface/grss/readme.go
+++ interface/grss/readme.go
@@ -11,7 +11,7 @@ import (
 func updateReadme() error {
 	items := []string{}
 	for _, v := range routers.Get() {
-		items = append(items, fmt.Sprintf("- [%s](/rss/xml%s.xml): %s", v.Path, v.Path, v.Source.Title))
+		items = append(items, fmt.Sprintf("- [%s](/xml%s.xml): %s", v.Path, v.Path, v.Source.Title))
 	}
 
 	data := fmt.Sprintf(`# Git RSS
