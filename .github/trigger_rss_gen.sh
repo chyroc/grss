be39fdf37f130a809b825d9a93d2f641a4a4d64a
chyroc <chyroc@qq.com>
2021-09-27 10:23:48 +0800

    fix dispatch action

--- .github/trigger_rss_gen.sh
+++ .github/trigger_rss_gen.sh
@@ -4,5 +4,5 @@ set -e
 
 curl -X POST https://api.github.com/repos/chyroc/rss/dispatches \
 -H 'Accept: application/vnd.github.everest-preview+json' \
--H "Authorization: token $GITHUB_TOKEN" \
+-H "Authorization: token $DISPATCH_ACTION_TOKEN" \
 --data '{"event_type": "opened"}'
